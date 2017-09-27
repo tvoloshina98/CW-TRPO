381295c0171910135984cc7d36cb6e106484ae05
Tatyana Voloshina <tvoloshina98@gmail.com>
2017-09-28 00:37:22 +0700

    Fix

--- src/function.c
+++ src/function.c
@@ -5,73 +5,78 @@
 #include <time.h> 
 #include "function.h"
 
-int *bcrandom(int *a, int *b, int *c, int *d)
+int bcrandom(int *a, int *b, int *c, int *d)
 {
     srand(time(NULL));
 
-
-    *a = 0 + rand() % 9;
-
-    do
-    {
+    do {
+        *a = 0 + rand() % 9;
         *b = 0 + rand() % 9;
-    } while (*b == *a);
-
-    do
-    {
         *c = 0 + rand() % 9;
-    } while (*c == *a || *c == *b);
-
-    do
-    {
         *d = 0 + rand() % 9;
-    } while (*d == *a || *d == *b || *d == *c);
+    } while (*d == *a || *d == *b || *d == *c || *c == *a || *c == *b || *b == *a);
     
-    printf("Rand number %d\n", *a * 1000 + *b * 100 + *c * 10 + *d);
+    printf("Rand number %d%d%d%d\n", *a, *b, *c, *d);
 
     return 0;
 
 }
 
-int *bulls_cows(int *a, int *b, int *c, int *d)
+int bulls_func(int *a, int *b, int *c, int *d, int *fst, int *scnd, int *thrd, int *frth)
+{
+	int bulls = 0;
+	
+	if (*fst == *a)
+		bulls++;
+    if (*scnd == *b)
+        bulls++;
+	if (*thrd == *c)
+        bulls++;
+	if (*frth == *d)
+        bulls++;
+        
+    return bulls;
+}
+
+int cows_func(int *a, int *b, int *c, int *d, int *fst, int *scnd, int *thrd, int *frth)
+{
+	int cows = 0;
+	
+	if (*fst == *b || *fst == *c || *fst == *d)
+		cows++;
+    if (*scnd == *a || *scnd == *c || *scnd == *d)
+	    cows++;
+    if (*thrd == *b || *thrd == *a || *thrd == *d)
+        cows++;
+    if (*frth == *b || *frth == *c || *frth == *a)
+        cows++;
+        
+    return cows;
+}
+
+int bulls_cows(int *a, int *b, int *c, int *d)
 {
     int number = 0, fst, scnd, thrd, frth;
-    int bulls = 0, cows = 0;
+    int bulls, cows;
 
     while (number != (*a * 1000 + *b * 100 + *c * 10 + *d))
     {
-        printf("Vvedite chetyrehznachnoe chislo = ");
+        printf("Enter the four-digit celery: = ");
         scanf("%d", &number); 
         printf("\n");
         fst = number / 1000;
         scnd = number / 100 % 10;
         thrd = number / 10 % 10;
         frth = number % 10;
+        
+        bulls = bulls_func(a, b, c, d, &fst, &scnd, &thrd, &frth);
+            
+        cows = cows_func(a, b, c, d, &fst, &scnd, &thrd, &frth);
 
-        if (fst == *a)
-            bulls++;
-        if (scnd == *b)
-            bulls++;
-        if (thrd == *c)
-            bulls++;
-        if (frth == *d)
-            bulls++;
-
-        if (fst == *b || fst == *c || fst == *d)
-            cows++;
-        if (scnd == *a || scnd == *c || scnd == *d)
-            cows++;
-        if (thrd == *b || thrd == *a || thrd == *d)
-            cows++;
-        if (frth == *b || frth == *c || frth == *a)
-            cows++;
-
-        printf("%d bull's\n", bulls);
-        printf("%d cow's\n", cows);
+        printf("%d bulls\n", bulls);
+        printf("%d cows\n", cows);
         printf("\n");
         printf("\n");
-        cows = 0;
-        bulls = 0;
     }
 
     printf("Congratulations\n");
--- src/main.c
+++ src/main.c
@@ -7,12 +7,9 @@
 
 void main()
 {
-    int *a, *b, *c, *d;
+    int a, b, c, d;
     
     bcrandom(&a, &b, &c, &d);
-
-    int number = 0, fst, scnd, thrd, frth;
-    int bulls = 0, cows = 0;
     
     bulls_cows(&a, &b, &c, &d);
 
