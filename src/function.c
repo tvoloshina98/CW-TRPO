efa5b80bc63ad36151cf08437076e7bc59e5cf4c
Tatyana Voloshina <tvoloshina98@gmail.com>
2017-09-28 00:33:19 +0700

    Create dir

--- src/function.c
+++ src/function.c
@@ -3,36 +3,42 @@
 #include <locale.h> 
 //#include <conio.h> 
 #include <time.h> 
+#include "function.h"
 
-void main()
+int *bcrandom(int *a, int *b, int *c, int *d)
 {
     srand(time(NULL));
-    int a, b, c, d;	//рандомим число по разрядам
 
 
-    a = 0 + rand() % 9;
-
-    do
-    {
-        b = 0 + rand() % 9;
-    } while (b == a);
+    *a = 0 + rand() % 9;
 
     do
     {
-        c = 0 + rand() % 9;
-    } while (c == a || c == b);
+        *b = 0 + rand() % 9;
+    } while (*b == *a);
 
     do
     {
-        d = 0 + rand() % 9;
-    } while (d == a || d == b || d == c);
+        *c = 0 + rand() % 9;
+    } while (*c == *a || *c == *b);
 
+    do
+    {
+        *d = 0 + rand() % 9;
+    } while (*d == *a || *d == *b || *d == *c);
+    
+    printf("Rand number %d\n", *a * 1000 + *b * 100 + *c * 10 + *d);
 
+    return 0;
+
+}
+
+int *bulls_cows(int *a, int *b, int *c, int *d)
+{
     int number = 0, fst, scnd, thrd, frth;
     int bulls = 0, cows = 0;
-    printf("Rand number %d\n", a * 1000 + b * 100 + c * 10 + d);
-    
-    while (number != (a * 1000 + b * 100 + c * 10 + d))
+
+    while (number != (*a * 1000 + *b * 100 + *c * 10 + *d))
     {
         printf("Vvedite chetyrehznachnoe chislo = ");
         scanf("%d", &number); 
@@ -42,22 +48,22 @@ void main()
         thrd = number / 10 % 10;
         frth = number % 10;
 
-        if (fst == a)
+        if (fst == *a)
             bulls++;
-        if (scnd == b)
+        if (scnd == *b)
             bulls++;
-        if (thrd == c)
+        if (thrd == *c)
             bulls++;
-        if (frth == d)
+        if (frth == *d)
             bulls++;
 
-        if (fst == b || fst == c || fst == d)
+        if (fst == *b || fst == *c || fst == *d)
             cows++;
-        if (scnd == a || scnd == c || scnd == d)
+        if (scnd == *a || scnd == *c || scnd == *d)
             cows++;
-        if (thrd == b || thrd == a || thrd == d)
+        if (thrd == *b || thrd == *a || thrd == *d)
             cows++;
-        if (frth == b || frth == c || frth == a)
+        if (frth == *b || frth == *c || frth == *a)
             cows++;
 
         printf("%d bull's\n", bulls);
@@ -70,6 +76,7 @@ void main()
 
     printf("Congratulations\n");
 
-    //_getch();
+    return 0;
+
 }
 
